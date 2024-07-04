a9b2bb913891b3b8ed58efe7662d5a54c2020ba0
JakSchlager <jami.schlager@gmail.com>
2024-07-04 11:20:22 +0200

    feat: #19 New Popup for when the user logs out

--- Melius/src/app/home-page/home-page.component.ts
+++ Melius/src/app/home-page/home-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {SideBarComponent} from "../side-bar/side-bar.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    SideBarComponent
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
--- Melius/src/app/register-form/register-form.component.ts
+++ Melius/src/app/register-form/register-form.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
