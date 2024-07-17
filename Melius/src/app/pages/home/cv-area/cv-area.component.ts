9194507e3815889d90e8e56350e58b0906aebe77
JakSchlager <jami.schlager@gmail.com>
2024-07-17 14:02:09 +0200

    feat: #5 added new forms to cv and fixed navbar

--- Melius/src/app/pages/home/cv-area/cv-area.component.ts
+++ Melius/src/app/pages/home/cv-area/cv-area.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {HomeNavbarComponent} from "../../../navigation/home-navbar/home-navbar.component";
 import {SideBarComponent} from "../../../navigation/side-bar/side-bar.component";
 import {NgOptimizedImage} from "@angular/common";
+import {MatDateRangeInput} from "@angular/material/datepicker";
 
 @Component({
   selector: 'app-cv-area',
@@ -9,7 +10,8 @@ import {NgOptimizedImage} from "@angular/common";
   imports: [
     HomeNavbarComponent,
     SideBarComponent,
-    NgOptimizedImage
+    NgOptimizedImage,
+    MatDateRangeInput
   ],
   templateUrl: './cv-area.component.html',
   styleUrl: './cv-area.component.css'
