8a383703c72f5ddec8adea4acbf688b0283e3ab7
JakSchlager <jami.schlager@gmail.com>
2024-07-18 12:02:36 +0200

    feat: #5 able to add more rows to education and job-experience form

--- Melius/src/app/pages/home/cv-area/cv-area.component.ts
+++ Melius/src/app/pages/home/cv-area/cv-area.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import {HomeNavbarComponent} from "../../../navigation/home-navbar/home-navbar.component";
 import {SideBarComponent} from "../../../navigation/side-bar/side-bar.component";
-import {NgOptimizedImage} from "@angular/common";
+import {NgForOf, NgOptimizedImage} from "@angular/common";
 import {MatDateRangeInput} from "@angular/material/datepicker";
+import {FormArray, FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-cv-area',
@@ -11,11 +13,66 @@ import {MatDateRangeInput} from "@angular/material/datepicker";
     HomeNavbarComponent,
     SideBarComponent,
     NgOptimizedImage,
-    MatDateRangeInput
+    MatDateRangeInput,
+    ReactiveFormsModule,
+    NgForOf,
+    MatIcon
   ],
   templateUrl: './cv-area.component.html',
   styleUrl: './cv-area.component.css'
 })
 export class CvAreaComponent {
+
+  constructor(private fb: FormBuilder) {}
+
+
+  //Ausbildungen
+  educationForm = this.fb.group({
+    educationFormItems: this.fb.array([])
+  });
+
+  get educationFormItems() {
+    return this.educationForm.get('educationFormItems') as FormArray;
+  }
+
+  deleteEducationInfo(index: number) {
+    this.educationFormItems.removeAt(index);
+  }
+
+  addEducationInfo() {
+    this.educationFormItems.push(
+      this.fb.group({
+        educationalInst: [''],
+        eIdateFrom: [''],
+        eIdateTo: [''],
+        eIFinished: ['']
+      })
+    )
+  }
+
+
+  //Berufserfahrungen
+  jobExperiencesForm = this.fb.group({
+    jobExperiencesFormItems: this.fb.array([])
+  });
+
+  get jobExperiencesFormItems() {
+    return this.jobExperiencesForm.get('jobExperiencesFormItems') as FormArray;
+  }
+
+  deleteJobExperiencesInfo(index: number) {
+    this.jobExperiencesFormItems.removeAt(index);
+  }
+
+  addJobExperiencesInfo() {
+    this.jobExperiencesFormItems.push(
+      this.fb.group({
+        companyName: [''],
+        workFrom: [''],
+        workTo: [''],
+        moreInfo: ['']
+      })
+    )
+  }
 
 }
