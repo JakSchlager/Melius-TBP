0ab2517a4ee739a79baaf435071a119eb5b6bff7
JakSchlager <jami.schlager@gmail.com>
2024-08-05 15:23:12 +0200

    remove .DS_Store files

--- frontend/src/app/single-components/dropdown-menu-home/dropdown-menu-home.component.ts
+++ frontend/src/app/single-components/dropdown-menu-home/dropdown-menu-home.component.ts
@@ -1,15 +1,32 @@
 import {Component, OnInit} from '@angular/core';
 import {FormArray, FormsModule} from "@angular/forms";
 import {DropdownModule} from "primeng/dropdown";
+import {NgClass, NgIf} from "@angular/common";
+import {identity} from "rxjs";
 
 
 
 @Component({
   selector: 'app-dropdown-menu-home',
   standalone: true,
-  imports: [FormsModule, DropdownModule],
+  imports: [FormsModule, DropdownModule, NgIf, NgClass],
   templateUrl: './dropdown-menu-home.component.html',
   styleUrl: './dropdown-menu-home.component.css'
 })
 export class DropdownMenuHomeComponent{
+  isOpen: boolean = false;
+  isAnimating: boolean = false;
+
+  toggleList() {
+    if (this.isOpen) {
+      this.isAnimating = true;
+    }
+    this.isOpen = !this.isOpen;
+  }
+
+  onAnimationEnd() {
+    if(!this.isOpen){
+      this.isAnimating = false;
+    }
+  }
 }
