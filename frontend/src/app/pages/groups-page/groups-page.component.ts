75f478fa0370719f3799a694e51d03ec1c9420c6
JakSchlager <jami.schlager@gmail.com>
2024-10-13 17:34:57 +0200

    fix: #11: new layout and routerLink

--- frontend/src/app/pages/groups-page/groups-page.component.ts
+++ frontend/src/app/pages/groups-page/groups-page.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, createComponent} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgIf, NgOptimizedImage} from "@angular/common";
+import {CreateOwnGroupComponent} from "../groups_subpages/create-own-group/create-own-group.component";
 
 @Component({
   selector: 'app-groups-page',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgOptimizedImage,
+    CreateOwnGroupComponent,
+    NgIf
   ],
   templateUrl: './groups-page.component.html',
   styleUrl: './groups-page.component.css'
 })
 export class GroupsPageComponent {
+  createGroupBtnPressed: boolean = false;
 
+  createGroup(): boolean {
+    if (!this.createGroupBtnPressed) {
+      this.createGroupBtnPressed = true;
+      return this.createGroupBtnPressed;
+    }
+
+    else {
+      this.createGroupBtnPressed = false;
+      return this.createGroupBtnPressed;
+    }
+  }
 }
--- frontend/src/app/pages/groups_subpages/create-own-group/create-own-group.component.ts
+++ frontend/src/app/pages/groups_subpages/create-own-group/create-own-group.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-create-own-group',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgOptimizedImage
+  ],
   templateUrl: './create-own-group.component.html',
   styleUrl: './create-own-group.component.css'
 })
