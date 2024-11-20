b2db90fb79e8823e3ea29620790274123d5c3193
JakSchlager <jami.schlager@gmail.com>
2024-11-20 15:02:57 +0100

    fix

--- frontend/src/app/pages/groups_subpages/create-group-form/create-own-group.component.ts
+++ frontend/src/app/pages/groups_subpages/create-group-form/create-own-group.component.ts
@@ -1,9 +1,10 @@
 import {Component, inject, Inject} from '@angular/core';
 import {RouterLink} from "@angular/router";
-import {NgOptimizedImage} from "@angular/common";
+import {NgIf, NgOptimizedImage} from "@angular/common";
 import {GroupAvatarComponent} from "../../../single-components/group-avatar/group-avatar.component";
 import {DropdownAvatarComponent} from "../../../single-components/user-avatar/dropdown-avatar.component";
 import {GroupsPageComponent} from "../../groups-page/groups-page.component";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-create-group-form',
@@ -12,12 +13,18 @@ import {GroupsPageComponent} from "../../groups-page/groups-page.component";
     RouterLink,
     NgOptimizedImage,
     GroupAvatarComponent,
-    DropdownAvatarComponent
+    DropdownAvatarComponent,
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './create-own-group.component.html',
   styleUrl: './create-own-group.component.css'
 })
 export class CreateOwnGroupComponent {
+
+  newGroupForm = new FormGroup({
+    groupName: new FormControl<string>('', Validators.required),
+  })
 
   constructor(@Inject(GroupsPageComponent) private groupsPage: GroupsPageComponent) {
   }
