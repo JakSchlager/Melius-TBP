d54ef97f667371f454e9d9f3d17da3cc640005ac
JakSchlager <jami.schlager@gmail.com>
2024-11-24 14:56:46 +0100

    feat: #37 new Filter & Role Buttons + Dropdown

--- frontend/src/app/single-components/create-roles/create-role.component.ts
+++ frontend/src/app/single-components/create-roles/create-role.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {HomePageServiceService} from "../../services/home-page-service.service";
 
 @Component({
   selector: 'app-create-roles',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    NgClass
+  ],
   templateUrl: './create-role.component.html',
   styleUrl: './create-role.component.css'
 })
-export class CreateRoleComponent {
+export class CreateRoleComponent implements OnInit {
+  testRoles: string[] = []
+  isOpen: boolean = false;
+  isAnimating: boolean = false;
 
+  ngOnInit(): void {
+    this.testRoles = [
+      "Administrator",
+      "Mitarbeiter",
+      "Scrum-Muster",
+      "Project-Owner",
+      "Frontend",
+      "Backend",
+      "DevOps"
+    ]
+  }
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  onAnimationEnd() {
+    if(!this.isOpen){
+      this.isAnimating = false;
+    }
+  }
 }
