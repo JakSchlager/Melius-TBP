009db8d50d66c16ecfc625391ca1908f72fe0389
JakSchlager <jami.schlager@gmail.com>
2024-11-27 14:19:09 +0100

    fix: #38 new settings page started

--- frontend/src/app/pages/settings-page/settings-page.component.ts
+++ frontend/src/app/pages/settings-page/settings-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-settings-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.css'
 })
