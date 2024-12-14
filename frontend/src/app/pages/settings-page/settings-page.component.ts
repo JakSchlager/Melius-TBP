26e388ff9674e6d66dda70e2333a738a6e215f0e
JakSchlager <jami.schlager@gmail.com>
2024-12-14 16:28:06 +0100

    feat: #38 new Background Selection

--- frontend/src/app/pages/settings-page/settings-page.component.ts
+++ frontend/src/app/pages/settings-page/settings-page.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import {LanguageSelectComponent} from "../../single-components/settings/language-select/language-select.component";
+import {ColorPickerModule} from "primeng/colorpicker";
+import {
+  BackgroundSelectComponent
+} from "../../single-components/settings/background-select/background-select.component";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-settings-page',
   standalone: true,
   imports: [
     MatIcon,
-    LanguageSelectComponent
+    LanguageSelectComponent,
+    ColorPickerModule,
+    BackgroundSelectComponent,
+    NgIf,
+    NgClass
   ],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.css'
 })
 export class SettingsPageComponent {
+  isBackgroundSelectionVisible = false; // Steuert die Sichtbarkeit des Menüs
+  isAnimating = false; // Steuert die Animation
 
+  openBackgroundSelection() {
+    this.isAnimating = true; // Animation beginnt
+    this.isBackgroundSelectionVisible = true; // Menü wird sichtbar
+  }
+
+  onAnimationEnd() {
+    if (!this.isBackgroundSelectionVisible) {
+      this.isAnimating = false; // Animation beendet
+    }
+  }
 }
--- frontend/src/app/single-components/settings/background-select/background-select.component.ts
+++ frontend/src/app/single-components/settings/background-select/background-select.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {SettingsPageComponent} from "../../../pages/settings-page/settings-page.component";
 
 @Component({
   selector: 'app-background-select',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    NgClass
+  ],
   templateUrl: './background-select.component.html',
   styleUrl: './background-select.component.css'
 })
 export class BackgroundSelectComponent {
+  isAnimating: boolean = false;
+  usedBackgrounds = [1, 2, 3, 4];
+
+  constructor(@Inject(SettingsPageComponent) public settingsPage: SettingsPageComponent) {}
+
+  closeBackgroundSelection() {
+    this.isAnimating = true; // Animation wird gestartet
+    this.settingsPage.isBackgroundSelectionVisible = false;
+  }
+
+  onAnimationEnd() {
+    this.isAnimating = false; // Animation beendet
+  }
 
 }
