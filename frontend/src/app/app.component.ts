1b60bf979c5c7dad4f20d261b65ab316a1a6f383
JakSchlager <jami.schlager@gmail.com>
2024-12-26 13:49:56 +0100

    feat: #38 language switch is now accessible - you can switch in app.component.ts file

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,16 @@
-import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
-import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
 import {SideBarComponent} from "./navigation/side-bar/side-bar.component";
-import {NgIf, NgOptimizedImage} from "@angular/common";
-import {MatDrawer, MatDrawerContainer, MatDrawerContent} from "@angular/material/sidenav";
-import {MatButton} from "@angular/material/button";
-import {MatIcon} from "@angular/material/icon";
-import {NotFoundComponent} from "./pages/not-found/not-found.component";
-import {filter} from "rxjs";
+import {NgIf} from "@angular/common";
+import {MatDrawer} from "@angular/material/sidenav";
+
+import {TranslateModule} from "@ngx-translate/core";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideBarComponent, NgIf, MatDrawerContent, MatIcon, MatDrawerContainer, MatButton, MatDrawer, NgOptimizedImage],
+  imports: [RouterOutlet, SideBarComponent, NgIf, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -19,7 +18,11 @@ export class AppComponent implements OnInit{
   title = 'Melius-TBP';
   @ViewChild('drawer') drawer!: MatDrawer;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+  }
 
   ngOnInit() {
 
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {provideHttpClient} from "@angular/common/http";
+import {HttpClient, provideHttpClient} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+  new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+
+    importProvidersFrom([TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    })])
+  ]
 };
