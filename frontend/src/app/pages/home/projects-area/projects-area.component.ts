9696198caccd3cc2116a67f7cfb13f0f343614ac
JakSchlager <jami.schlager@gmail.com>
2025-02-18 13:53:28 +0100

    feat: updated documentation about the design + style changes in projects and strengths

--- frontend/src/app/pages/home/projects-area/projects-area.component.ts
+++ frontend/src/app/pages/home/projects-area/projects-area.component.ts
@@ -1,17 +1,11 @@
-import {Component, ElementRef, inject, OnInit, signal, ViewChild} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import $ from 'jquery';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {NgClass, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
-import {MatIcon} from "@angular/material/icon";
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {ProfileService} from "../../../services/profile.service";
-import {isEmpty} from "rxjs";
-import {Profile} from "../../../interfaces/profile";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {Router} from "@angular/router";
 import {TranslatePipe} from "@ngx-translate/core";
 import {PortfolioService} from "../../../services/portfolio.service";
 import {GHRepo} from "../../../interfaces/GHRepo";
-import {Portfolio} from "../../../interfaces/Portfolio";
 import {GhReposService} from "../../../services/gh-repos.service";
 
 
@@ -21,9 +15,7 @@ import {GhReposService} from "../../../services/gh-repos.service";
   imports: [
     ReactiveFormsModule,
     NgIf,
-    MatIcon,
     NgForOf,
-    NgOptimizedImage,
     NgClass,
     TranslatePipe
   ],
@@ -38,6 +30,7 @@ export class ProjectsAreaComponent implements OnInit {
   portfolioService: PortfolioService = inject(PortfolioService);
   ghRepoService: GhReposService = inject(GhReposService);
   buttonClass = "hidden"
+  showGhTitle: string = "";
 
   ngOnInit() {
     setTimeout(() => {
@@ -210,7 +203,6 @@ export class ProjectsAreaComponent implements OnInit {
           });
 
           this.ghRepoService.addAllRepos(repos).subscribe()
-
           this.reloadPage();
         }
       });
@@ -250,8 +242,9 @@ window.toggleRepo = function (index: number) {
     repoLinkButton.classList.toggle('cursor-not-allowed');
     if (repoElement.classList.contains('blur-sm')) {
       toggleBtn.innerHTML = `
-        <svg class="mt-2 mr-2" xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#00000">
-          <path d="M607-627q29 29 42.5 66t9.5 76q0 15-11 25.5T622-449q-15 0-25.5-10.5T586-485q5-26-3-50t-25-41q-17-17-41-26t-51-4q-15 0-25.5-11T430-643q0-15 10.5-25.5T466-679q38-4 75 9.5t66 42.5Zm-127-93q-19 0-37 1.5t-36 5.5q-17 3-30.5-5T358-742q-5-16 3.5-31t24.5-18q23-5 46.5-7t47.5-2q137 0 250.5 72T904-534q4 8 6 16.5t2 17.5q0 9-1.5 17.5T905-466q-18 40-44.5 75T802-327q-12 11-28 9t-26-16q-10-14-8.5-30.5T753-392q24-23 44-50t35-58q-50-101-144.5-160.5T480-720Zm0 520q-134 0-245-72.5T60-463q-5-8-7.5-17.5T50-500q0-10 2-19t7-18q20-40 46.5-76.5T166-680l-83-84q-11-12-10.5-28.5T84-820q11-11 28-11t28 11l680 680q11 11 11.5 27.5T820-84q-11 11-28 11t-28-11L624-222q-35 11-71 16.5t-73 5.5ZM222-624q-29 26-53 57t-41 67q50 101 144.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
         </svg>
       `;
       repoLinkButton.innerHTML = `
@@ -259,8 +252,8 @@ window.toggleRepo = function (index: number) {
       `
     } else {
       toggleBtn.innerHTML = `
-        <svg class="mt-2 mr-2" xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#00000">
-          <path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-134 0-244.5-72T61-462q-5-9-7.5-18.5T51-500q0-10 2.5-19.5T61-538q64-118 174.5-190T480-800q134 0 244.5 72T899-538q5 9 7.5 18.5T909-500q0 10-2.5 19.5T899-462q-64 118-174.5 190T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
         </svg>
       `;
     }
